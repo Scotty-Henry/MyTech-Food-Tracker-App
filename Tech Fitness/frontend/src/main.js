48c66e288067e9380c9562909b7ac785e013b586
Scotty Henry <scottyhenry@me.com>
2019-08-06 14:28:01 -0400

    fushion chart dependencies deleted, vue chart and dependencies added

--- Tech Fitness/frontend/src/main.js
+++ Tech Fitness/frontend/src/main.js
@@ -1,27 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
+// required bootstrap dependencies
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-// required fushion chart dependencies
-import FusionCharts from 'fusioncharts';
-import Charts from 'fusioncharts/fusioncharts.charts';
-import Widgets from 'fusioncharts/fusioncharts.widgets';
-import PowerCharts from 'fusioncharts/fusioncharts.powercharts';
-import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
-import VueFusionCharts from 'vue-fusioncharts';
+Vue.use(BootstrapVue)
 
-Charts(FusionCharts);
-PowerCharts(FusionCharts);
-Widgets(FusionCharts);
-FusionTheme(FusionCharts);
-
-Vue.use(VueFusionCharts, FusionCharts);
+// required eCharts dependency
+import "@/plugins/echarts";
 
 Vue.config.productionTip = false
-Vue.use(BootstrapVue)
+
 
 require("./assets/style.css");
 
