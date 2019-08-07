406a8a63a50b586903f26a9a072496bb03e2eca0
Steven Marshall <steven.marshall@zoho.com>
2019-08-07 09:30:33 -0400

    Refined API calls

--- Tech Fitness/frontend/src/food-service.js
+++ Tech Fitness/frontend/src/food-service.js
@@ -11,26 +11,24 @@ const apiClient = axios.create({
 
 export default {
 
-    // returns list of foods based on search
-    getFoods() {
-        return apiClient.get(`/search/?format=json&api_key=${process.env.VUE_APP_CURR_API_KEY}`).then(response => response.data);
+    // returns list of foods based on a search query
+    getFoods(query) {
+        return apiClient.get(`/search/?format=json&q=${query}&ds=Standard%20Reference&api_key=${process.env.VUE_APP_CURR_API_KEY}`).then(response => response.data);
     },
     // returns a single food by ndbno, with 'basic' attributes (cal, prot, fat, carb, sodium)
     getFood(id) {
         return apiClient.get(`/reports/?format=json&ndbno=${id}&type=b&api_key=${process.env.VUE_APP_CURR_API_KEY}`).then(response => response.data);
     },
-    getReview(id) {
-        return apiClient.get('/' + id).then(response => response.data);
-    },
-    deleteReview(id) {
-        return apiClient.delete('/' + id);
-    },
-    addNewReview(review) {
-        return apiClient.post('', JSON.stringify(review));
-    },
-    updateReview(id, review) {
-        return apiClient.put('/' + id, JSON.stringify(review));
-    }
-
-
+    // getReview(id) {
+    //     return apiClient.get('/' + id).then(response => response.data);
+    // },
+    // deleteReview(id) {
+    //     return apiClient.delete('/' + id);
+    // },
+    // addNewReview(review) {
+    //     return apiClient.post('', JSON.stringify(review));
+    // },
+    // updateReview(id, review) {
+    //     return apiClient.put('/' + id, JSON.stringify(review));
+    // }
 }
