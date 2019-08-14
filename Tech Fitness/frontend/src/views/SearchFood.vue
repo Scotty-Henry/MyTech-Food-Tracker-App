0813bafd94311ee308137cde82a97d8c23bbdf47
Scotty Henry <scottyhenry@me.com>
2019-08-14 16:34:40 -0400

    search food page progress

--- Tech Fitness/frontend/src/views/SearchFood.vue
+++ Tech Fitness/frontend/src/views/SearchFood.vue
@@ -3,7 +3,7 @@
     <b-row>
       <b-col>
         <div id="search-food" class="text-center">
-          <form class="form-signin form-group mt-5" @submit.prevent="handleSubmit">
+          <form id="foodsearchform" class="form-signin mt-1" @submit.prevent="handleSubmit">
             <h1 class="h3 mb-3 font-weight-normal">Find a food!</h1>
             <label for="name" class="sr-only" id="food-search">Food name</label>
             <input
@@ -15,43 +15,11 @@
               required
               autofocus
             />
-            <button type="submit" id="searchbutton" class="btn btn-success btn-md">Search!</button>
-          </form>
-          <form class="form-signin form-group mt-5" @submit.prevent="handleNDBNO">
-            <label for="meal-form-ndbno" class="sr-only" id="add-food">Add by ndbno</label>
-            <input
-              id="meal-form-ndbno"
-              type="text"
-              class="form-control"
-              placeholder="ndbno"
-              v-model="foodItem.ndbno"
-              autofocus
-            />
-            <label for="meal-form-qty" class="sr-only" id="add-qty">Qty</label>
-            <input
-              id="meal-form-qty"
-              type="text"
-              class="form-control"
-              placeholder="Qty."
-              v-model="foodItem.qty"
-              autofocus
-            />
-            <button type="submit" id="foodbutton" class="btn btn-success btn-md">Add Food!</button>
+            <b-button block type="submit" id="searchbutton" class="btn btn-success btn-md">Search!</b-button>
           </form>
 
-          <div>Food:  {{this.foodItem.name}} </div>
-          <div>My food's cals: {{this.foodItem.cal}} </div>
-          <div>My food's pro: {{this.foodItem.pro}} </div>
-          <div>My food's fat: {{this.foodItem.fat}} </div>
-          <div>My food's carbs: {{this.foodItem.carb}} </div>
-
-          <meal id="meal" :foodArray="this.foodArray"></meal>
-          </div>
-      </b-col>
-      <b-col>
-        <div id="table" class="text-left">
+          <div id="table" class="text-center">
             <b-table
-            sticky-header
             selectable
             select-mode="single"
             striped
@@ -62,8 +30,56 @@
             @row-selected="rowSelected"
             responsive="sm">
             </b-table>
+         </div>
         </div>
       </b-col>
+      <b-col id="mealcol">
+        <div id="search-food" class="text-center justify-content-center align-items-center">
+          <form id="ndbnosearchform" class="form-signin mt-1" @submit.prevent="handleNDBNO">
+            <h1 class="h3 mb-3 font-weight-normal">Track Food!</h1>
+              <label for="meal-form-qty" class="sr-only" id="add-qty">Qty</label>
+                <input
+                  id="meal-form-qty"
+                  type="text"
+                  class="form-control"
+                  placeholder="Qty."
+                  v-model="foodItem.qty"
+                  autofocus
+                />
+                <b-button block type="submit" id="foodbutton" class="btn btn-success btn-md">Add Food!</b-button>
+          </form>
+            <!-- <label for="meal-form-ndbno" class="sr-only" id="add-food">Add by ndbno</label>
+              <input 
+                id="meal-form-ndbno"
+                type="text"
+                class="form-control"
+                placeholder="ndbno"
+                v-model="foodItem.ndbno"
+                autofocus
+              /> -->
+        <div>
+            <b-card
+              id="foodcard"
+              tag="article"
+              style="max-width: 30rem;"
+              class="mb-2"
+              bg-variant="dark" 
+              header="Food Item Info:"
+              text-variant="white"
+            >
+
+              <div> {{this.foodItem.name}} </div>
+              <br>
+              <div>My food's cals: {{this.foodItem.cal}} </div>
+              <div>My food's pro: {{this.foodItem.pro}} </div>
+              <div>My food's fat: {{this.foodItem.fat}} </div>
+              <div>My food's carbs: {{this.foodItem.carb}} </div>
+            </b-card>
+        </div>
+      </div>
+          <meal id="meal" :foodArray="this.foodArray"></meal>
+
+       </b-col>
     </b-row>
 </b-container>
   
@@ -96,7 +112,7 @@ export default {
         name: "",
         results: [], 
       },
-      fields: ["name", "ndbno"]
+      fields: ["name"]
     };
   },
   methods: {
@@ -109,7 +125,7 @@ export default {
 
     rowSelected(items) {
       this.selected = items;
-      this.foodItem.ndbno = items.ndbno;
+      this.foodItem = items[0];
     },
     
     handleNDBNO() {
@@ -141,23 +157,34 @@ export default {
 <style>
 #search-food {
   color: whitesmoke !important;
+  margin-top: 7.5%;
+  justify-content: center;
 }
 #table {
-  padding-top: 5%;
-  margin-top: 5%;
-  max-height: 130vh;
+  margin-top: 3.5%;
+  max-height: 60vh;
   overflow: auto;
 }
+#foodsearchform {
+  display: inline;
+}
+#ndbnosearchform {
+  display: inline;
+}
 #foodbutton {
-  margin: .5em;
+  display: inline;
+  margin-top: 2%;
 }
 #searchbutton {
-  margin: .5em;
+  display: inline;
+  margin-top: 2%;
 }
 ::-webkit-scrollbar {
-display: none;
+  display: none;
 }
-/* .table {
-    color:whitesmoke !important;
-} */
+#foodcard {
+  margin-top: 5%;
+  justify-content: center;
+}
+
 </style>
