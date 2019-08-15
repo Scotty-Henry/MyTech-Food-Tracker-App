d3271d526eb72ff2d834ac9d8e9d56caaf21d18f
Scotty Henry <scottyhenry@me.com>
2019-08-15 14:29:21 -0400

    History Table Complete

--- Tech Fitness/frontend/src/views/HistoryView.vue
+++ Tech Fitness/frontend/src/views/HistoryView.vue
@@ -1,67 +1,67 @@
 
 <template>
-  <div id="history-container" class="container">   
+  <div id="history-container" class="container">
     <div id="history-food" class="text-center">
-
-      <history id="history" :userMeals="this.userMeals"></history>
-      <!-- Need to make each item clickable?
-      <div id="table" class="text-left">
-          <b-table dark :items="food.results" :fields="fields" responsive="sm"></b-table>
+      <!-- <div id="table" class="text-left">
+        <b-table dark :items="userMeals" :fields="fields" responsive="sm"></b-table>
       </div> -->
-    </div>
-</div>
   
+      <b-table dark :items="allFood" responsive="sm"></b-table>
+
+    </div>
+  </div>
 </template>
 
 <script>
 import TFService from "@/TFService.js";
-import History from '@/components/History';
+import History from "@/components/History";
 
 export default {
   name: "HistoryView",
   components: {
-    History,
+    History
   },
   data() {
     return {
-            userMeals: [],
-            }
+      userMeals: [],
+      fields: ['date', 'meal_category', 'foods'],
+      allFood: []
+    };
   },
-  methods: {
-    
-   
-  },
-  created()
-    {
-      TFService.getMealbyUser().then((data) => {
-            data.forEach((mealObj) => { 
-              let meal = 
-                  { 
-                  date: TFService.stringtoDate(mealObj.date),
-                  meal_category: mealObj.meal_category,
-                  userID: mealObj.userID,
-                  foods: [  ], 
-                  }  
-                  mealObj.foods.forEach((food) => {
-                                let foodItem = {
-                                  ndbno: food.ndbno,
-                                  name: food.name, 
-                                  cal: food.cal,
-                                  carb: food.carb,
-                                  fat: food.fat,
-                                  pro: food.pro,
-                                  qty: food.qty,
-                                  unit: food.unit,
-                                  }
-                                  meal.foods.push(foodItem);
-                                });
-                    
-            this.userMeals.push(meal);      
-      })
-       console.log(this.userMeals) 
-    })
-    }
- 
+  methods: {},
+  created() {
+    let foodItem=null;
+    TFService.getMealbyUser().then(data => {
+      data.forEach(mealObj => {
+        let meal = {
+          date: TFService.stringtoDate(mealObj.date),
+          meal_category: mealObj.meal_category,
+          userID: mealObj.userID,
+          foods: []
+        };
+        mealObj.foods.forEach(food => {
+           foodItem = {
+            date: meal.date,
+            category: meal.meal_category,
+            name: food.name,
+            ndbno: food.ndbno,
+            cal: food.cal,
+            carb: food.carb,
+            fat: food.fat,
+            pro: food.pro,
+            qty: food.qty,
+            unit: food.unit
+          };
+          meal.foods.push(foodItem);
+        });
+
+        this.userMeals.push(meal);
+        this.allFood.push(foodItem);
+        
+      });
+      console.log(this.userMeals);
+    });
+  }
 };
 </script>
 
@@ -72,5 +72,4 @@ export default {
 #table {
   padding-top: 5%;
 }
-
 </style>
